7efb99a9cb533338bd397aa86f03a173a4975b20
Doxy4 <93224478+Doxy4@users.noreply.github.com>
2021-12-11 00:06:44 +0200

    Add files via upload

---
+++ qb-trashsearch/server/main.lua
@@ -0,0 +1,30 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterServerEvent('qb-trashsearch:server:startDumpsterTimer')
+AddEventHandler('qb-trashsearch:server:startDumpsterTimer', function(dumpster)
+    startTimer(source, dumpster)
+end)
+
+RegisterNetEvent('qb-trashsearch:server:recieveItem', function(item, itemAmount)
+    local src = source
+    local ply = QBCore.Functions.GetPlayer(src)
+    ply.Functions.AddItem(Config.RewardsSmall[item].item, itemAmount)
+end)
+
+RegisterNetEvent('qb-trashsearch:server:givemoney', function(money)
+local src = source
+local ply = QBCore.Functions.GetPlayer(src)
+ply.Functions.AddMoney("cash", money)
+end)
+
+function startTimer(id, object)
+    local timer = 10 * 1000
+
+    while timer > 0 do
+        Wait(10)
+        timer = timer - 10
+        if timer == 0 then
+            TriggerClientEvent('qb-trashsearch:server:removeDumpster', id, object)
+        end
+    end
+end
